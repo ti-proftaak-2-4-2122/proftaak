1bcc2dfd87b334204df42912b2e298f723aace4c
Twan Hogervorst <twanhog@gmail.com>
2022-05-27 13:43:27 +0200

    [Add] Example and Template UnitTest

---
+++ tests/template/test.cpp
@@ -0,0 +1,15 @@
+//
+// Test Template
+// Created by twanh on 27-5-2022.
+//
+
+#define TEST_SUCCESS 0
+#define TEST_FAILED 1
+
+int main() {
+
+//    if(test == false)
+//        return TEST_FAILED;
+
+    return TEST_SUCCESS;
+}
