aab984536213c9584e2a2af474c13ca2f8bda362
Twan Hogervorst <twanhog@gmail.com>
2022-06-01 11:40:49 +0200

    [Update] Documentation for ParentTransform class

--- modules/ECS/include/ParentTransform.h
+++ modules/ECS/include/ParentTransform.h
@@ -13,14 +13,30 @@
 class GameObject;
 class Transform;
 
+/**
+ * @brief Component used for positioning a GameObject relative to its parent
+ */
 class ParentTransform : public Component
 {
 private:
     GameObject* parent;
 public:
+    /**
+     * Default constructor, requires a reference to a parent GameObject
+     * @param parent Reference to the parent GameObject used for positioning
+     */
     ParentTransform(GameObject* parent);
 
+    /**
+     * Gets the transform component of the parent
+     * @return The parent transform
+     */
     inline Transform& GetParentTransform();
+
+    /**
+     * Gets the calculated model matrix of the parent, based on it's current transform
+     * @return The parent model matrix
+     */
     glm::mat4 GetParentModelMatrix();
 };
 
