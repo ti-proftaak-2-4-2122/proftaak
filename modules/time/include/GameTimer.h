b3a9d95ec9941da856ffb4251643786dbf98bd45
Twan Hogervorst <twanhog@gmail.com>
2022-06-01 13:04:08 +0200

    [Add] Documentation for GameTimer

--- modules/time/include/GameTimer.h
+++ modules/time/include/GameTimer.h
@@ -8,14 +8,25 @@
 #ifndef GAMETIMER_H
 #define GAMETIMER_H
 
+/**
+ * @brief Class for keeping track of the time between 2 frames
+ */
 class GameTimer
 {
 private:
     inline static double lastTime = 0;
 
 public:
+    /**
+     * Variable containing the delta time from the previous to the current frame
+     */
     inline static float deltatime = 0.0f;
 
+    /**
+     * Updates the internal timer and delta time with the new time
+     * @param time Time since program start or a timestamp.
+     * Must be larger than internal timer and non-negative.
+     */
     static void update(double time);
 
 };
