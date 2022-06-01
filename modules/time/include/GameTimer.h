82f16118e4f1435472362f72f10907c033a2fa48
Twan Hogervorst <twanhog@gmail.com>
2022-06-01 14:58:52 +0200

    [Update] Big improvements GameTimer, now passes unittest

--- modules/time/include/GameTimer.h
+++ modules/time/include/GameTimer.h
@@ -2,20 +2,37 @@
 // Created by Ewout on 24-5-2022.
 //
 
-#ifndef GAMETIMER_H
-#define GAMETIMER_H
+#pragma once
 
 class GameTimer
 {
 private:
-    inline static double lastTime = 0;
+    inline static double currentTime = 0.0f;
+    inline static float deltaTime = 0.0f;
 
 public:
-    inline static float deltatime = 0.0f;
 
+    /**
+     * Updates the start time of the current frame and calculates the delta time
+     * @param time Time in seconds, must be a timestamp or offset from the start of the program
+     */
     static void update(double time);
 
+    /**
+     * Resets the GameTimer to 0
+     */
+    static void reset();
+
+    /**
+     * Gets the start time of the current frame
+     * @return Time in seconds
+     */
+    static double getCurrentTime();
+
+    /**
+     * Gets the delta time between the previous and the current frame
+     * @return Time in seconds
+     */
+    static float getDeltaTime();
+
 };
-
-
-#endif // GAMETIMER_H
