6b15d81a5110c645b2be9ec3eb90fd3ade1ac5b3
Daan <daanvd1000@gmail.com>
2022-06-10 10:25:00 +0200

    [add] colour palette

---
+++ colours.h
@@ -0,0 +1,25 @@
+//
+// Created by Daan van Donk on 10/06/2022.
+//
+#pragma once
+
+#define YELLOW_SUNFLOWER glm::vec3(1, 0.764f, 0.070f);
+#define YELLOW_RADAINT glm::vec3(0.968f, 0.623f, 0.121f);
+#define ORANGE_PUFFINS glm::vec3(0.933f, 0.352f, 0.141f);
+#define PIGMENT_RED glm::vec3(0.917f, 0.125f, 0.152f);
+#define GREEN_ERENOS glm::vec3(0.768f, 0.898f, 0.219f);
+#define GREEN_ANDROID glm::vec3(0.639f, 0.796f, 0.219f);
+#define GREEN_GRASS glm::vec3(0, 0.580f, 0.196f);
+#define TEAL_TURKISH glm::vec3(0, 0.384f, 0.4f);
+#define BLUE_MARTIN glm::vec3(0.070f, 0.796f, 0.768f);
+#define BLUE_MEDITERRANIAN glm::vec3(0.070f, 0.537f, 0.654f);
+#define BLUE_MARINE glm::vec3(0.023f, 0.321f, 0.866f);
+#define BLUE_SEA glm::vec3(0.105f, 0.078f, 0.392f);
+#define PINK_ROSE glm::vec3(0.992f, 0.654f, 0.874f);
+#define PINK_LAVENDER glm::vec3(0.850f, 0.501f, 0.980f);
+#define PURPLE_FORGOTTEN glm::vec3(0.6f, 0.501f, 0.980f);
+#define PURPLE_RING glm::vec3(0.341f, 0.345f, 0.733f);
+#define RED_BARRA glm::vec3(0.929f, 0.298f, 0.403f);
+#define RED_BERRY glm::vec3(0.709f, 0.203f, 0.443f);
+#define PURPLE_HOLLY glm::vec3(0.513f, 0.203f, 0.443f);
+#define PURPLE_MAGENTA glm::vec3(0.435f, 0.117f, 0.317f);
